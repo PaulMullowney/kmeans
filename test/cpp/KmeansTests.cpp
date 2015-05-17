1e016e1c8738a057eb9f0e53d0819a9e3efb8ed0
Paul <paulmullowney@gmail.com>
2015-05-17 17:57:53 -0600

    Making the test harness a little more flexible.

--- test/cpp/KmeansTests.cpp
+++ test/cpp/KmeansTests.cpp
@@ -1,10 +1,21 @@
 #include "kmeans.hpp"
 #include "gtest/gtest.h"
 
+int my_argc;
+char** my_argv;
+
 class KmeansTests : public testing::Test {
 
  protected: 
 
+  string fileName;
+  int m;
+  int n;
+  int k;
+  int nIters;
+  int useCublas;
+  float rtol;
+  
   /* @brief constructor */
   KmeansTests() {
   }
@@ -16,7 +27,25 @@ class KmeansTests : public testing::Test {
   /* @brief this method will be called before each test is run. You
      should define it if you need to initialize the varaibles.
      Otherwise, this can be skipped. */
-  virtual void SetUp() {}
+  virtual void SetUp() {
+    if (my_argc>1) {
+      fileName = string(my_argv[1]);
+      m = atoi(my_argv[2]);
+      n = atoi(my_argv[3]);
+      k = atoi(my_argv[4]);
+      nIters = atoi(my_argv[5]);
+      useCublas = atoi(my_argv[6]);
+      rtol = atof(my_argv[7]);
+    } else {
+      fileName = string("mat-sift");
+      m = 898790;
+      n = 128;
+      k = 256;
+      nIters = 50;
+      useCublas = 1;
+      rtol = 1.e-5;
+    }
+  }
 
   /* @brief this method will be called after each test is run.
      You should define it if there is cleanup work to do. Otherwise,
@@ -25,15 +54,9 @@ class KmeansTests : public testing::Test {
 };
 
 
-TEST_F(KmeansTests, SIFT_CUBLAS) {
+TEST_F(KmeansTests, TEST) {
 
-  string fileName("mat-sift");
-  int m = 898790;
-  int n = 128;
-  int k = 1024;
-  int nIters = 50;
   kmeansErrorStatus err = KMEANS_SUCCESS;
-  int useCublas = 1;
 
   /* allocate data */
   float * data = (float *) malloc(m*n*sizeof(float));
@@ -49,7 +72,7 @@ TEST_F(KmeansTests, SIFT_CUBLAS) {
   memset(centers, 0, sizeof(float)*k*n);
 
   /* run kmeans */
-  err = computeKmeansF(data,m,n,k,1.e-5,nIters,1,0,useCublas,centers);
+  err = computeKmeansF(data,m,n,k,rtol,nIters,1,0,useCublas,centers);
   if (err!=KMEANS_SUCCESS) {
     cout << "Kmeans(CUBLAS) internal error '" << kmeansGetErrorString(err) << "'" <<  endl;
   }
@@ -58,115 +81,14 @@ TEST_F(KmeansTests, SIFT_CUBLAS) {
   if (data) free(data);
   if (centers) free(centers);
 }
-
-
-TEST_F(KmeansTests, SIFT) {
-
-  string fileName("mat-sift");
-  int m = 898790;
-  int n = 128;
-  int k = 1024;
-  int nIters = 50;
-  kmeansErrorStatus err = KMEANS_SUCCESS;
-  int useCublas = 0;
-
-  /* allocate data */
-  float * data = (float *) malloc(m*n*sizeof(float));
-  float * centers = (float *) malloc(k*n*sizeof(float));
-
-  /* read matrix from file */
-  FILE * fid = fopen(fileName.c_str(),"rb");
-  int nread = fread(data, sizeof(float), m*n, fid);
-  ASSERT_EQ(nread, m*n);
-  fclose(fid);
-
-  /* initialize centers to 0 */
-  memset(centers, 0, sizeof(float)*k*n);
-
-  /* run kmeans */
-  err = computeKmeansF(data,m,n,k,1.e-5,nIters,1,0,useCublas,centers);
-  if (err!=KMEANS_SUCCESS) {
-    cout << "Kmeans internal error '" << kmeansGetErrorString(err) << "'" <<  endl;
-  }
-
-  /* free data */
-  if (data) free(data);
-  if (centers) free(centers);
-}
-
-TEST_F(KmeansTests, HOG_CUBLAS) {
-  
-  string fileName("mat-hog");
-  int m = 796160;
-  int n = 324;
-  int k = 1024;
-  int nIters = 50;
-  kmeansErrorStatus err = KMEANS_SUCCESS;
-  int useCublas = 1;
-
-  /* allocate data */
-  float * data = (float *) malloc(m*n*sizeof(float));
-  float * centers = (float *) malloc(k*n*sizeof(float));
-
-  /* read matrix from file */
-  FILE * fid = fopen(fileName.c_str(),"rb");
-  int nread = fread(data, sizeof(float), m*n, fid);
-  ASSERT_EQ(nread, m*n);
-  fclose(fid);
-
-  /* initialize centers to 0 */
-  memset(centers, 0, sizeof(float)*k*n);
-
-  /* run kmeans */
-  err = computeKmeansF(data,m,n,k,1.e-5,nIters,1,0,useCublas,centers);
-  if (err!=KMEANS_SUCCESS) {
-    cout << "Kmeans(CUBLAS) internal error '" << kmeansGetErrorString(err) << "'" <<  endl;
-  }
-
-  /* free data */
-  if (data) free(data);
-  if (centers) free(centers);
-}
-
-
-TEST_F(KmeansTests, HOG) {
-  
-  string fileName("mat-hog");
-  int m = 796160;
-  int n = 324;
-  int k = 1024;
-  int nIters = 50;
-  kmeansErrorStatus err = KMEANS_SUCCESS;
-  int useCublas = 0;
-
-  /* allocate data */
-  float * data = (float *) malloc(m*n*sizeof(float));
-  float * centers = (float *) malloc(k*n*sizeof(float));
-
-  /* read matrix from file */
-  FILE * fid = fopen(fileName.c_str(),"rb");
-  int nread = fread(data, sizeof(float), m*n, fid);
-  ASSERT_EQ(nread, m*n);
-  fclose(fid);
-
-  /* initialize centers to 0 */
-  memset(centers, 0, sizeof(float)*k*n);
-
-  /* run kmeans */
-  err = computeKmeansF(data,m,n,k,1.e-5,nIters,1,0,useCublas,centers);
-  if (err!=KMEANS_SUCCESS) {
-    cout << "Kmeans internal error '" << kmeansGetErrorString(err) << "'" <<  endl;
-  }
-
-  /* free data */
-  if (data) free(data);
-  if (centers) free(centers);
-}
-
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
+  if (argc>1) {
+    my_argc = argc;
+    my_argv = argv;
+  }
   int err = RUN_ALL_TESTS();
-  int keepOpen = getchar();
   return err;
 }
+
